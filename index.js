3ae5e44320bdae3951d4d2802bc3f29afcfaa0cb
CHIC043 <karsen.kapayou@students.msswarriors.org>
2026-04-17 15:29:54 +0000

    check

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
-//Add code below
+let quoteLength = quote.length;
+let twentySecondLetter = quote[21];
+let output = `${quoteLength} [${twentySecondLetter}]`;
+document.getElementById("result").innerHTML = output;
